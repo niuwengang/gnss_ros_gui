a332caeee5a4fa003c0ae34e6dd5a013ae2d5cc9
gang <niu_wengang@163.com>
2022-11-29 16:33:21 +0800

    add simple ui

--- src/gnss_ros_gui/include/gnss_ros_gui/qnode.hpp
+++ src/gnss_ros_gui/include/gnss_ros_gui/qnode.hpp
@@ -1,23 +1,7 @@
-/**
- * @file /include/gnss_ros_gui/qnode.hpp
- *
- * @brief Communications central!
- *
- * @date February 2011
- **/
-/*****************************************************************************
-** Ifdefs
-*****************************************************************************/
-
 #ifndef gnss_ros_gui_QNODE_HPP_
 #define gnss_ros_gui_QNODE_HPP_
 
-/*****************************************************************************
-** Includes
-*****************************************************************************/
 
-// To workaround boost/qt4 problems that won't be bugfixed. Refer to
-//    https://bugreports.qt.io/browse/QTBUG-22829
 #ifndef Q_MOC_RUN
 #include <ros/ros.h>
 #endif
@@ -25,49 +9,20 @@
 #include <QThread>
 #include <QStringListModel>
 
-
-/*****************************************************************************
-** Namespaces
-*****************************************************************************/
-
 namespace gnss_ros_gui {
-
-/*****************************************************************************
-** Class
-*****************************************************************************/
 
 class QNode : public QThread {
     Q_OBJECT
 public:
-	QNode(int argc, char** argv );
-	virtual ~QNode();
-	bool init();
-	bool init(const std::string &master_url, const std::string &host_url);
-	void run();
-
-	/*********************
-	** Logging
-	**********************/
-	enum LogLevel {
-	         Debug,
-	         Info,
-	         Warn,
-	         Error,
-	         Fatal
-	 };
-
-	QStringListModel* loggingModel() { return &logging_model; }
-	void log( const LogLevel &level, const std::string &msg);
-
+    QNode(int argc, char** argv );
+    virtual ~QNode();
+    bool init();
+    void run();
 Q_SIGNALS:
-	void loggingUpdated();
-    void rosShutdown();
-
 private:
-	int init_argc;
-	char** init_argv;
-	ros::Publisher chatter_publisher;
-    QStringListModel logging_model;
+    int init_argc;
+    char** init_argv;
+    ros::Publisher chatter_publisher;
 };
 
 }  // namespace gnss_ros_gui
